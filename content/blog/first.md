eb0116cc67baadfc796ecfa7615753a5f519ac79
Luís Nabais <gitlab.duwqnj9l@nabais.eu>
2024-10-23 11:44:52 +0100

    Cleaned config

--- content/blog/first.md
+++ content/blog/first.md
@@ -1,7 +1,7 @@
 Title: First Article
 Date: 2024-10-22 10:00
 Category: Test Category
-Slug: first-article-slug
+Slug: first-article
 Authors: Luís Nabais
 Summary: A brief summary of the article
 
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,85 +1,77 @@
+# Site Information
 AUTHOR = 'Luís Nabais'
 SITENAME = 'Luís Nabais'
-#SITEURL = 'https://luisnabais.github.io'
-OUTPUT_PATH = 'output/'
-
-# Theme settings
-THEME = 'themes/nabais'
-#THEME = 'Flex'
 SITETITLE = 'Luís Nabais'
 SITESUBTITLE = ' 🇵🇹 DevOps engineer, Linux specialist and trainer.<br />Passionate about Open Source, running, bodyweight fitness and travelling.'
 SITEDESCRIPTION = ''
-SITELOGO = '/images/profile.png'  # Add path to your logo if you have one
+SITELOGO = '/images/profile.png'
 
-
+# Locale and Timezone
 TIMEZONE = 'Europe/Lisbon'
 DEFAULT_LANG = 'en'
 
+# Theme Settings
+THEME = 'themes/nabais'
+STYLESHEET_URL = 'theme/css/style.css'
+
+# Paths and URLs
 PATH = 'content'
 PAGE_PATHS = ['pages']
-ARTICLE_PATHS = ['blog']  # If you have any articles, put them in a separate folder
+ARTICLE_PATHS = ['blog']
+STATIC_PATHS = ['images', 'extra']
 
-# URL and save_as settings
 PAGE_URL = '{slug}.html'
 PAGE_SAVE_AS = '{slug}.html'
 ARTICLE_URL = 'blog/{slug}.html'
 ARTICLE_SAVE_AS = 'blog/{slug}.html'
 
-# Disable unnecessary features
+# Disable features
 AUTHOR_SAVE_AS = ''
 AUTHORS_SAVE_AS = ''
 CATEGORY_SAVE_AS = ''
 CATEGORIES_SAVE_AS = ''
-#TAG_SAVE_AS = ''
-#TAGS_SAVE_AS = ''
-
-# Ensure no auto-generated index
+TAG_SAVE_AS = ''
+TAGS_SAVE_AS = ''
 INDEX_SAVE_AS = ''
 
-
-# Static paths
-STATIC_PATHS = ['images', 'extra']
-
-
-
-# Feed generation is usually not desired when developing
+# Feed Settings
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (
-    ('Home', '/'),
-    ('Blog', '/blog'),
-)
-
-# Social widget
-SOCIAL = (
-    ("linkedin", "https://linkedin.com/in/luisnabais"),
-    ("mastodon", "https://mastodon.social/@luisnabais"),
-    ("x-twitter",        "https://x.com/luis_nabais"),
-    ("github",   "https://github.com/luisnabais"),
-    ("gitlab",   "https://gitlab.com/luisnabais"),
-)
-
+# Navigation
 MAIN_MENU = True
 MENUITEMS = (
     ('Blog', '/blog'),
 )
 
+LINKS = (
+    ('Home', '/'),
+    ('Blog', '/blog'),
+)
+
+SOCIAL = (
+    ("linkedin", "https://linkedin.com/in/luisnabais"),
+    ("mastodon", "https://mastodon.social/@luisnabais"),
+    ("x-twitter", "https://x.com/luis_nabais"),
+    ("github",   "https://github.com/luisnabais"),
+    ("gitlab",   "https://gitlab.com/luisnabais"),
+)
+
+# Pagination
 DEFAULT_PAGINATION = 10
 
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
 
-
-# Content variables
-#SITESUBTITLE = 'site_subtitle_variable'
-STYLESHEET_URL = 'theme/css/style.css'
-
-
+# Templates
 DIRECT_TEMPLATES = ['index', 'blog_index']
 BLOG_INDEX_SAVE_AS = 'blog/index.html'
 PAGINATED_TEMPLATES = {'index': None, 'blog_index': None}
+
+# Uncomment to use relative URLs in development
+# RELATIVE_URLS = True
+
+# Uncomment these lines when ready to publish
+# SITEURL = 'https://nabais.eu'
+# OUTPUT_PATH = 'output/'
