e58126d1f2553aef62f1d78943e68c349db98841
Luís Nabais <gitlab.duwqnj9l@nabais.eu>
2024-10-23 12:19:02 +0100

    Fixed publish settings, to fix GitHub actions domain override

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,10 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://nabais.eu"
+SITEURL = 'https://nabais.eu'
+DOMAIN = SITEURL
+FEED_DOMAIN = SITEURL
+HTTPS = True
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
